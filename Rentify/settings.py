ebe97e7c0c8fb02498e2e058931ae9158686e062
Anjali Sharma <anjalisharma.020305>
2026-01-30 08:45:26 +0530

    Set DEBUG=False for production

--- Rentify/settings.py
+++ Rentify/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!wduip490e7f0tdb5he-$1icee%*_=o1mveet8-2ot8vxu87nz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rentify-urne.onrender.com']
 
